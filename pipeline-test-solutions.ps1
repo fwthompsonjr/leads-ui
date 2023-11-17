74f230056f94241f168a8e19f5463c8cf6d6cd64
Frank Thompson <Frank.Thompson@wolterskluwer.com>
2023-11-17 11:47:44 -0600

    modification to continuous quality action

--- pipeline-test-solutions.ps1
+++ pipeline-test-solutions.ps1
@@ -47,7 +47,8 @@ function generateExecutionCommand( $solution ) {
 $startedAt = [datetime]::UtcNow
 $currentDir = [System.IO.Path]::GetDirectoryName( $MyInvocation.MyCommand.Path );
 $errorsFile = [System.IO.Path]::Combine( $currentDir, "test-solution-error-file.txt" );
-$settingsFile = [System.IO.Path]::Combine( $currentDir, "CodeCoverage.runsettings" );
+$settingsFile = [System.IO.Path]::Combine( $currentDir, "src" );
+$settingsFile = [System.IO.Path]::Combine( $settingsFile, "CodeCoverage.runsettings" );
 $di = [System.IO.DirectoryInfo]::new( $currentDir );
 $found = $di.GetFiles($searchPattern, [System.IO.SearchOption]::AllDirectories) | Where-Object { $_.Name.IndexOf( 'integration' ) -lt 0 }
 $commands = @();
