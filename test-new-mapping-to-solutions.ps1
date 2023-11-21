cd888554bdd12472e08d2db8daa7d4b5a7621d96
Frank Thompson <Frank.Thompson@wolterskluwer.com>
2023-11-21 10:10:53 -0600

    change coverage testing to use solutions

---
+++ test-new-mapping-to-solutions.ps1
@@ -0,0 +1,7 @@
+﻿$searchPattern = "*.sln"
+$currentDir = [System.IO.Path]::GetDirectoryName( $MyInvocation.MyCommand.Path );
+$errorsFile = [System.IO.Path]::Combine( $currentDir, "test-solution-error-file.txt" );
+$settingsFile = [System.IO.Path]::Combine( $currentDir, "src" );
+$settingsFile = [System.IO.Path]::Combine( $settingsFile, "CodeCoverage.runsettings" );
+$di = [System.IO.DirectoryInfo]::new( $currentDir );
+$di.GetFiles($searchPattern, [System.IO.SearchOption]::AllDirectories) | Where-Object { $_.Name.IndexOf( 'integration' ) -lt 0 }
