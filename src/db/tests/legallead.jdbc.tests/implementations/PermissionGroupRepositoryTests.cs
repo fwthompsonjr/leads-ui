c87afeca7fdf3fe24348e2f22d4add6caaa4b8c0
Frank Thompson <frank.thompson.jr@gmail.com>
2024-01-01 22:15:18 -0600

    adding test coverage

---
+++ src/db/tests/legallead.jdbc.tests/implementations/PermissionGroupRepositoryTests.cs
@@ -0,0 +1,24 @@
+﻿using legallead.jdbc.helpers;
+using legallead.jdbc.implementations;
+using legallead.jdbc.interfaces;
+using Moq;
+
+namespace legallead.jdbc.tests.implementations
+{
+    public class PermissionGroupRepositoryTests
+    {
+        private readonly DataContext dataContext;
+
+        public PermissionGroupRepositoryTests()
+        {
+            dataContext = new DataContext(new Mock<IDapperCommand>().Object);
+        }
+
+        [Fact]
+        public void RepoCanConstruct()
+        {
+            var repo = new PermissionGroupRepository(dataContext);
+            Assert.NotNull(repo);
+        }
+    }
+}
