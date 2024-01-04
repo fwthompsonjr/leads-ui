2dc38bf2afd540868a2d5059014ff9e12b1e10ff
Frank Thompson <frank.thompson.jr@gmail.com>
2024-01-03 21:45:39 -0600

    added method to serialize and post data to server

---
+++ src/presentation/legallead.desktop/js/JsPermissionChange.cs
@@ -0,0 +1,39 @@
+﻿using CefSharp.Wpf;
+using legallead.desktop.entities;
+using legallead.desktop.interfaces;
+using legallead.desktop.utilities;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace legallead.desktop.js
+{
+    internal class JsPermissionChange : JsHandler
+    {
+        private readonly IPermissionApi? permissionApi;
+        private readonly UserBo? user;
+
+        private bool HasBrowser => permissionApi != null &&
+            user != null &&
+            web != null &&
+            web.CanExecuteJavascriptInMainFrame;
+
+        public JsPermissionChange(ChromiumWebBrowser? browser) : base(browser)
+        {
+            var provider = AppBuilder.ServiceProvider;
+            permissionApi = provider?.GetService<IPermissionApi>();
+            user = provider?.GetService<UserBo>();
+        }
+
+        public override void Submit(string formName, string json)
+        {
+            if (!HasBrowser || user == null) return;
+            var name = PermissionForms.Find(p => p.Equals(formName, StringComparison.OrdinalIgnoreCase));
+            if (name == null) { return; }
+            Console.WriteLine(json);
+        }
+    }
+}
