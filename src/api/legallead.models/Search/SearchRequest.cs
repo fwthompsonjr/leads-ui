08ca33cdce6a516c57df62aa8912fe5c5963ab7a
Frank Thompson <frank.thompson.jr@gmail.com>
2024-01-13 19:33:11 -0600

    setup mapping user request to search

---
+++ src/api/legallead.models/Search/SearchRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace legallead.models.Search
+{
+    public class SearchRequest
+    {
+        public int WebId { get; set; }
+        [Required]
+        public string State { get; set; } = string.Empty;
+        [Required]
+        public string County { get; set; } = string.Empty;
+        [Required]
+        [DataType(DataType.Date)]
+        public string StartDate { get; set; } = string.Empty;
+        [Required]
+        [DataType(DataType.Date)]
+        public string EndDate { get; set; } = string.Empty;
+    }
+}
--- src/api/legallead.search.api/WebMapper.cs
+++ src/api/legallead.search.api/WebMapper.cs
@@ -1,6 +1,50 @@
-﻿namespace legallead.search.api
+﻿using AutoMapper;
+using legallead.models.Search;
+using legallead.permissions.api.Model;
+
+namespace legallead.search.api
 {
-    public class WebMapper
+    public static class WebMapper
     {
+
+        private static MapperConfiguration? _configuration;
+        private static IMapper? _mapper;
+
+        private static MapperConfiguration Configuration => _configuration ??= GetConfiguration();
+        private static IMapper Mapper => _mapper ??= Configuration.CreateMapper();
+
+        private static MapperConfiguration GetConfiguration()
+        {
+            return new MapperConfiguration(c =>
+            {
+                c.CreateMap<UserSearchRequest, SearchRequest>()
+                    .ForMember(x => x.WebId, opt => opt.MapFrom(y => GetWebIndex(y.County.Name, y.State)))
+                    .ForMember(x => x.County, opt => opt.MapFrom(y => y.County.Name))
+                    .ForMember(x => x.State, opt => opt.MapFrom(y => y.State))
+                    .ForMember(x => x.StartDate, opt => opt.MapFrom(y => UnixDateToString(y.StartDate)))
+                    .ForMember(x => x.EndDate, opt => opt.MapFrom(y => UnixDateToString(y.EndDate)));
+            });
+        }
+        private static int GetWebIndex(string? county, string? st)
+        {
+            if(string.IsNullOrWhiteSpace(county) || 
+                string.IsNullOrWhiteSpace(st)) return 0;
+            var lookup = $"{st.ToLower()}-{county.Replace(' ', '-').ToLower()}";
+            if (lookup.Equals("tx-collin")) return 20;
+            if (lookup.Equals("tx-tarrant")) return 10;
+            return 0;
+        }
+        private static string UnixDateToString(long unixTime)
+        {
+            var dte = DateTimeOffset.FromUnixTimeMilliseconds(unixTime).Date;
+            return dte.ToString("yyyy-MM-dd");
+        }
+
+        public static T? MapFrom<TK, T>(TK source) where T : class, new()
+        {
+            if (source == null) return default;
+            return Mapper.Map<T>(source);
+        }
+
     }
 }
