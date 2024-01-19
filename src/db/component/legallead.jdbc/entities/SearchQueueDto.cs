bf55784ff7260b328fb52d0126a6aae1cd2394ad
Frank Thompson <frank.thompson.jr@gmail.com>
2024-01-19 09:05:08 -0600

    update queue repository interface

---
+++ src/db/component/legallead.jdbc/entities/SearchQueueDto.cs
@@ -0,0 +1,30 @@
+﻿namespace legallead.jdbc.entities
+{
+    [TargetTable(TableName = "SEARCHQUEUE")]
+    public class SearchQueueDto : SearchDto
+    {
+        public string? Payload { get; set; }
+
+        public override object? this[string field]
+        {
+            get
+            {
+                var fieldName = FieldList.Find(x => x.Equals(field, Comparison));
+                if (fieldName == null) return null;
+                if (fieldName.Equals("Payload", Comparison)) return Payload;
+                return base[field];
+            }
+            set
+            {
+                var fieldName = FieldList.Find(x => x.Equals(field, Comparison));
+                if (fieldName == null) return;
+                if (fieldName.Equals("Payload", Comparison))
+                {
+                    Payload = ChangeType<string>(value) ?? string.Empty;
+                    return;
+                }
+                base[field] = value;
+            }
+        }
+    }
+}
