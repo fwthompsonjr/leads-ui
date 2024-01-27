f4eecb567ded47047176201fb0abca3aebc1cf7e
Frank Thompson <frank.thompson.jr@gmail.com>
2024-01-27 16:22:39 -0600

    set up configuration based db context - error db

--- src/logging/legallead.logging/LoggingDb.cs
+++ src/logging/legallead.logging/LoggingDb.cs
@@ -1,32 +1,22 @@
-﻿namespace legallead.logging
+﻿using legallead.jdbc.models;
+using Newtonsoft.Json;
+
+namespace legallead.logging
 {
     internal static class LoggingDb
     {
-        private const string Endpoint = "database-leads.cmmu8tkizri9.us-east-2.rds.amazonaws.com";
-        private const string PostGresCommand = "server=<localhost>;user=<username>;password=<password>;Database=wlogpermissions;port=3306;";
-
-        private static string[] GetPassCode()
+        public static string GetConnectionString(string connectionType = "Local", string databaseName = "error")
         {
-            const string saltLocal = "legal.lead.awsd.passcode";
-            const string conversion = "+mnWfJqr8rejN8Gi1GcnibZj+/zTaRqWvP5oQ8uCwwU=";
-            const string vector = "X+jEaHnsFfQ3IZgF23b12w==";
-            return CryptoContent.Decrypt(conversion, saltLocal, vector).Split('|');
+            ErConnectProvider.Target = connectionType;
+            return ErConnectProvider.ConnectionString(databaseName);
         }
 
-        public static string GetConnectionString()
+        private static ErConnect ErConnectProvider => _provider ??= GetConnect();
+        private static ErConnect? _provider;
+        private static readonly string ErConnectString = Properties.Resources.connectionstring_json;
+        private static ErConnect GetConnect()
         {
-            if (!string.IsNullOrWhiteSpace(_dbconnection))
-                return _dbconnection;
-
-            var secret = GetPassCode();
-            var connection = PostGresCommand;
-            connection = connection.Replace("<localhost>", $"{Endpoint}");
-            connection = connection.Replace("<username>", secret[0]);
-            connection = connection.Replace("<password>", secret[1]);
-            _dbconnection = connection;
-            return _dbconnection;
+            return JsonConvert.DeserializeObject<ErConnect>(ErConnectString) ?? new();
         }
-
-        private static string? _dbconnection;
     }
 }
