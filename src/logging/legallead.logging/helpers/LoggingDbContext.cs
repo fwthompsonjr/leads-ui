b4d9339e3dd745914b2a354ac27ee163388cb407
Frank Thompson <frank.thompson.jr@gmail.com>
2024-01-29 08:29:00 -0600

    set connection string to member variable

--- src/logging/legallead.logging/helpers/LoggingDbContext.cs
+++ src/logging/legallead.logging/helpers/LoggingDbContext.cs
@@ -1,13 +1,15 @@
 ﻿using legallead.logging.interfaces;
 using MySqlConnector;
+using System;
 using System.Data;
 
 namespace legallead.logging.helpers
 {
     public class LoggingDbContext : ILoggingDbContext
     {
-        private readonly string _connectionString;
         private readonly ILoggingDbCommand _dbexecutor;
+        private readonly string EnvironmentName;
+        private readonly string DatabaseName;
 
         public LoggingDbContext(ILoggingDbCommand command) : this(command, environ, db)
         {
@@ -15,7 +17,8 @@ namespace legallead.logging.helpers
 
         public LoggingDbContext(ILoggingDbCommand command, string environment, string database)
         {
-            _connectionString = LoggingDb.GetConnectionString(environment, database);
+            EnvironmentName = environment;
+            DatabaseName = database;
             _dbexecutor = command;
         }
 
@@ -23,7 +26,8 @@ namespace legallead.logging.helpers
 
         public virtual IDbConnection CreateConnection()
         {
-            return new MySqlConnection(_connectionString);
+            var connectionString = LoggingDb.GetConnectionString(EnvironmentName, DatabaseName);
+            return new MySqlConnection(connectionString);
         }
         private const string environ = "test";
         private const string db = "error";
