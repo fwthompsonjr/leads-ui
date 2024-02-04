299a16c49a39c3479ecf6a40e5d81c50f33272ed
Frank Thompson <frank.thompson.jr@gmail.com>
2024-02-04 17:14:34 -0600

    denton county search change to get case details

---
+++ src/core/legallead.core/component/records.search/Classes/DentonCaseStyleSummary.cs
@@ -0,0 +1,10 @@
+﻿namespace legallead.records.search.Classes
+{
+    public class DentonCaseStyleSummary
+    {
+        public string? CaseStyle { get; set; }
+        public string? DateFiled { get; set; }
+        public string? Court { get; set; }
+        public string? CaseType { get; set; }
+    }
+}
