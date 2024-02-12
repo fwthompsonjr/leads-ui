382989bc1124b5a1ce71da806c82c9b901a12021
Frank Thompson <frank.thompson.jr@gmail.com>
2024-02-11 18:48:46 -0600

    push content change to support api

--- src/api/legallead.permissions.api/Utility/PaymentHtmlTranslator.cs
+++ src/api/legallead.permissions.api/Utility/PaymentHtmlTranslator.cs
@@ -1,14 +1,21 @@
-﻿using legallead.permissions.api.Interfaces;
+﻿using legallead.jdbc.interfaces;
+using legallead.permissions.api.Interfaces;
 
 namespace legallead.permissions.api.Utility
 {
     public class PaymentHtmlTranslator : IPaymentHtmlTranslator
     {
+        private readonly IUserSearchRepository _repo;
+        public PaymentHtmlTranslator(IUserSearchRepository db)
+        { 
+            _repo = db;
+        }
         public bool IsRequestValid(string? status, string? id)
         {
             if (string.IsNullOrWhiteSpace(status)) return false;
             if (string.IsNullOrWhiteSpace(id)) return false;
             if (!requestNames.Contains(status)) return false;
+            
             return true;
         }
 
