84f2733de5957f15e7a92e4924d6f6ee3822daa7
Frank Thompson <frank.thompson.jr@gmail.com>
2024-03-04 09:26:01 -0600

    adding payment customer infrastructure

---
+++ src/db/component/legallead.jdbc/entities/PaymentCustomerInsert.cs
@@ -0,0 +1,15 @@
+﻿using Dapper;
+
+namespace legallead.jdbc.entities
+{
+    public class PaymentCustomerInsert : PaymentCustomerQuery
+    {
+        public string? CustomerId { get; set; }
+        internal override DynamicParameters GetParameters()
+        {
+            var parameters = base.GetParameters();
+            parameters.Add("customer_index", CustomerId);
+            return parameters;
+        }
+    }
+}
---
+++ src/db/component/legallead.jdbc/interfaces/ICustomerRepository.cs
@@ -0,0 +1,11 @@
+﻿using legallead.jdbc.entities;
+
+namespace legallead.jdbc.interfaces
+{
+    public interface ICustomerRepository
+    {
+        Task<bool> DoesCustomerExist(PaymentCustomerQuery query);
+        Task<PaymentCustomerBo?> GetCustomer(PaymentCustomerQuery query);
+        Task<KeyValuePair<bool, string>> AddCustomer(PaymentCustomerInsert dto);
+    }
+}
