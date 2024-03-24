5b553e1d3bf06a6f3a15a2bc6da2fd096d1814f8
Frank Thompson <frank.thompson.jr@gmail.com>
2024-03-24 11:06:49 -0500

    testing workflow to set release note

---
+++ x-pipeline-update-release-notes.ps1
@@ -0,0 +1,21 @@
+﻿param(
+    [string]
+    $version = "3.2.0"
+)
+
+$startedAt = [datetime]::UtcNow
+$workfolder = [System.IO.Path]::GetDirectoryName( $MyInvocation.MyCommand.Path );
+$readmeFile = [System.IO.Path]::Combine( $workfolder, "README.md" );
+if( [system.io.file]::Exists( $readmeFile ) -eq $false ) {
+    return;
+}
+$find1 = "## vX.Y.Z";
+$find2 = "- ReleaseDate"
+$mdcontent = [System.IO.File]::ReadAllText( $readmeFile );
+$dte = [string]::Concat( " ( ", [DateTime]::UtcNow.ToString("s").Replace("T", " "), " )");
+if ( $mdcontent.IndexOf( $find1 ) -ge 0 ) {
+    $mdcontent = $mdcontent.Replace( $find1, $version );
+}
+if ( $mdcontent.IndexOf( $find2 ) -ge 0 ) {
+    $mdcontent = $mdcontent.Replace( $find2, $dte );
+}
