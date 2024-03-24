85339c404bb37a2983efe28306a7560da88579b5
Frank Thompson <frank.thompson.jr@gmail.com>
2024-03-24 11:57:41 -0500

    modify publish windows workflows

--- x-pipeline-update-release-notes.ps1
+++ x-pipeline-update-release-notes.ps1
@@ -3,7 +3,6 @@
     $version = "3.2.0"
 )
 
-$startedAt = [datetime]::UtcNow
 $workfolder = [System.IO.Path]::GetDirectoryName( $MyInvocation.MyCommand.Path );
 $readmeFile = [System.IO.Path]::Combine( $workfolder, "README.md" );
 if( [system.io.file]::Exists( $readmeFile ) -eq $false ) {
@@ -11,11 +10,19 @@ if( [system.io.file]::Exists( $readmeFile ) -eq $false ) {
 }
 $find1 = "## vX.Y.Z";
 $find2 = "- ReleaseDate"
-$mdcontent = [System.IO.File]::ReadAllText( $readmeFile );
+$mdContent = [System.IO.File]::ReadAllText( $readmeFile );
+$hasChanged = $false;
 $dte = [string]::Concat( " ( ", [DateTime]::UtcNow.ToString("s").Replace("T", " "), " )");
-if ( $mdcontent.IndexOf( $find1 ) -ge 0 ) {
-    $mdcontent = $mdcontent.Replace( $find1, $version );
+if ( $mdContent.IndexOf( $find1 ) -ge 0 ) {
+    $mdContent = $mdContent.Replace( $find1, [string]::Concat( "## v", $version ));
+    $hasChanged = $true;
 }
-if ( $mdcontent.IndexOf( $find2 ) -ge 0 ) {
-    $mdcontent = $mdcontent.Replace( $find2, $dte );
+if ( $mdContent.IndexOf( $find2 ) -ge 0 ) {
+    $mdContent = $mdContent.Replace( $find2, $dte );
+    $hasChanged = $true;
+}
+
+if ($hasChanged -eq $true ) {
+    [System.IO.File]::Delete( $readmeFile ) | Out-Null;
+    [System.IO.File]::WriteAllText( $readmeFile, $mdContent );
 }
