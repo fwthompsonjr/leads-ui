a28ffa7aec6ee2a27d523533f2d2aaaefcc7bf09
Frank Thompson <frank.thompson.jr@gmail.com>
2024-04-20 18:41:34 -0500

    code coverage - presentation

---
+++ src/presentation/legallead.desktop.tests/utilities/DownloadStatusMessagingTests.cs
@@ -0,0 +1,29 @@
+﻿using Bogus;
+using legallead.desktop.utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace legallead.desktop.tests.utilities
+{
+    public class DownloadStatusMessagingTests
+    {
+        [Theory]
+        [InlineData(0)]
+        [InlineData(100)]
+        [InlineData(200)]
+        [InlineData(206)]
+        [InlineData(400)]
+        [InlineData(401)]
+        [InlineData(402)]
+        [InlineData(422)]
+        public void SutCanGetStatusMessage(int status)
+        {
+            var description = new Faker().Lorem.Sentence(12);
+            var message = DownloadStatusMessaging.GetMessage(status, description);
+            Assert.NotEmpty(message);
+        }
+    }
+}
