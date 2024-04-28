5a8191a11b4fc61a74447bd4b5686a4315f7d333
Frank Thompson <frank.thompson.jr@gmail.com>
2024-04-28 00:53:26 -0500

    setup test to validate email content

---
+++ src/email/component/implementations/AccountRegistrationEmailTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace legallead.email.implementations
+{
+    internal class AccountRegistrationEmailTests
+    {
+    }
+}
