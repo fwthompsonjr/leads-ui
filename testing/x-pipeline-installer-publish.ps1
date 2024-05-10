c94e21c71a91f496df5050217f20bc4a1be58bb9
Frank Thompson <frank.thompson.jr@gmail.com>
2024-05-10 14:06:05 -0500

    edit to zip file output location

--- testing/x-pipeline-installer-publish.ps1
+++ testing/x-pipeline-installer-publish.ps1
@@ -3,18 +3,14 @@
     $version = "3.2.0"
 )
 
-function echoZipFileName {
-    try {
-        Write-Output "ZIPFILE: $zipFileName is created."
-        echo "ZIPFILE_NAME=$zipFileName" >> $env:GITHUB_ENV
-    } catch { }
-}
 $startedAt = [datetime]::UtcNow
 $workfolder = [System.IO.Path]::GetDirectoryName( $MyInvocation.MyCommand.Path );
+$rootfolder = [System.IO.Path]::GetDirectoryName( $workfolder );
+
 $binfolder = [System.IO.Path]::Combine( $workfolder, "installation-suite/scripts" );
 $project = "legallead.zip.installation"
 $zipFileName = [string]::Concat($project, "-", $version, ".zip");
-$zipFileName = [System.IO.Path]::Combine( $workfolder, $zipFileName );
+$zipFileName = [System.IO.Path]::Combine( $rootfolder, $zipFileName );
 
 if( [System.IO.Directory]::Exists( $binfolder ) -eq $false ) {
     throw "$project script folder is not found!"
@@ -32,5 +28,5 @@ if( [system.io.file]::Exists( $zipFileName ) -eq $false ) {
     [Environment]::ExitCode = 1000;
     return 1000;
 } else {
-    echoZipFileName
+    Write-Output "ZIPFILE: $zipFileName is created."
 }
