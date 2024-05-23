c8be3924d7c526f389efe7f4588be98c9dba686c
Frank Thompson <frank.thompson.jr@gmail.com>
2024-05-23 12:15:38 -0500

    fix: add test parameters

---
+++ mysql/testing/account-profile-test-setup.sql
@@ -0,0 +1,10 @@
+/*
+Profile.Change Name - should complete successfully
+Profile.Change Name - should validate input
+Profile.Change Name - should save input
+Profile.Change Address - Mailing and Billing should complete successfully
+Profile.Change Address - Mailing only should complete successfully
+Profile.Change Address - Billing only should complete successfully
+Profile.Change Address - should validate input
+Profile.Change Address - should save input
+*/
