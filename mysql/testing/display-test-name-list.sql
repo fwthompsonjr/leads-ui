9baa293acef4d9befb0faf93253f06db2f30ad97
Frank Thompson <frank.thompson.jr@gmail.com>
2024-05-28 11:15:58 -0500

    testing - account profile - parameters

---
+++ mysql/testing/display-test-name-list.sql
@@ -0,0 +1,19 @@
+-- display tests defined in suite
+SET @testSuite = 'Profile';
+SET @locator = CONCAT( @testSuite, '%' );
+SELECT nn.*, p.`Test`
+FROM (
+	SELECT 
+		n.Id, n.`Name`
+	FROM (
+		SELECT nn.*, CONCAT( nn.`Name`, '%' ) Findex
+		FROM testing.TESTNAMES nn
+		WHERE nn.`Name` = @testSuite) n
+	) nn
+CROSS
+JOIN ( SELECT `Test` 
+		  FROM testing.TESTPARAMETER 
+          WHERE `Test` 
+          LIKE @locator
+         GROUP BY `Test` ) p 
+ORDER BY p.`Test`
--- mysql/testing/display-test-parameters.sql
+++ mysql/testing/display-test-parameters.sql
@@ -1,9 +1,12 @@
 /*
 	create a list of email accounts that are used for specific test cases
-    
+    Profile.Change Address - should complete successfully
+	Profile.Change Email - should complete successfully
+	Profile.Change Name - should complete successfully
+	Profile.Change Phone - should complete successfully
 */
 SET SQL_SAFE_UPDATES = 0;
-SET @testName = 'Account%';
+SET @testName = 'Profile.Change%';
 
 
 SELECT * 
