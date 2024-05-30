771ed9f3b63c271caf94b0de5d501000495d0599
Frank Thompson <frank.thompson.jr@gmail.com>
2024-05-29 22:00:40 -0500

    dto - discounts - setup payment amount calculation

---
+++ src/db/component/legallead.jdbc/entities/DiscountPaymentDto.cs
@@ -0,0 +1,49 @@
+﻿namespace legallead.jdbc.entities
+{
+    public class DiscountPaymentDto : BaseDto
+    {
+        public string? UserId { get; set; }
+        public string? ExternalId { get; set; }
+        public string? LevelName { get; set; }
+        public string? PriceType { get; set; }
+        public decimal? Price { get; set; }
+        public DateTime? CompletionDate { get; set; }
+        public DateTime? CreateDate { get; set; }
+
+        public override object? this[string field]
+        {
+            get
+            {
+                if (field == null) return null;
+                var fieldName = FieldList.Find(x => x.Equals(field, Comparison));
+                if (fieldName == null) return null;
+                if (fieldName.Equals("Id", Comparison)) return Id;
+                if (fieldName.Equals("UserId", Comparison)) return UserId;
+                if (fieldName.Equals("ExternalId", Comparison)) return ExternalId;
+                if (fieldName.Equals("LevelName", Comparison)) return LevelName;
+                if (fieldName.Equals("PriceType", Comparison)) return PriceType;
+                if (fieldName.Equals("Price", Comparison)) return Price;
+                if (fieldName.Equals("CompletionDate", Comparison)) return CompletionDate;
+                return CreateDate;
+            }
+            set
+            {
+                if (field == null) return;
+                var fieldName = FieldList.Find(x => x.Equals(field, Comparison));
+                if (fieldName == null) return;
+                if (fieldName.Equals("Id", Comparison))
+                {
+                    Id = ChangeType<string>(value) ?? string.Empty;
+                    return;
+                }
+                if (fieldName.Equals("UserId", Comparison)) { UserId = ChangeType<string?>(value); return; }
+                if (fieldName.Equals("ExternalId", Comparison)) { ExternalId = ChangeType<string?>(value); return; }
+                if (fieldName.Equals("LevelName", Comparison)) { LevelName = ChangeType<string?>(value); return; }
+                if (fieldName.Equals("PriceType", Comparison)) { PriceType = ChangeType<string?>(value); return; }
+                if (fieldName.Equals("Price", Comparison)) { Price = ChangeType<decimal?>(value); return; }
+                if (fieldName.Equals("CompletionDate", Comparison)) { CompletionDate = ChangeType<DateTime?>(value); }
+                if (fieldName.Equals("CreateDate", Comparison)) { CreateDate = ChangeType<DateTime?>(value); }
+            }
+        }
+    }
+}
---
+++ src/db/tests/legallead.jdbc.tests/entities/DiscountPaymentDtoTests.cs
@@ -0,0 +1,99 @@
+﻿using Bogus;
+using legallead.jdbc.entities;
+
+namespace legallead.jdbc.tests.entities
+{
+    public class DiscountPaymentDtoTests
+    {
+
+        private static readonly Faker<DiscountPaymentDto> faker =
+            new Faker<DiscountPaymentDto>()
+            .RuleFor(x => x.Id, y => y.Random.Guid().ToString("D"))
+            .RuleFor(x => x.UserId, y => y.Random.Guid().ToString("D"))
+            .RuleFor(x => x.ExternalId, y => y.Hacker.Phrase())
+            .RuleFor(x => x.LevelName, y => y.Hacker.Phrase())
+            .RuleFor(x => x.PriceType, y => y.Hacker.Phrase())
+            .RuleFor(x => x.Price, y => y.Random.Decimal(1, 50))
+            .RuleFor(x => x.CompletionDate, y => y.Date.Recent())
+            .RuleFor(x => x.CreateDate, y => y.Date.Recent());
+
+
+        [Fact]
+        public void DiscountPaymentDtoCanBeCreated()
+        {
+            var exception = Record.Exception(() =>
+            {
+                _ = new DiscountPaymentDto();
+            });
+            Assert.Null(exception);
+        }
+
+        [Fact]
+        public void DiscountPaymentDtoCanBeGenerated()
+        {
+            var exception = Record.Exception(() =>
+            {
+                _ = faker.Generate();
+            });
+            Assert.Null(exception);
+        }
+
+        [Fact]
+        public void DiscountPaymentDtoCanSetId()
+        {
+            var data = faker.Generate(2);
+            var src = data[0];
+            var dest = data[1];
+            dest.Id = src.Id;
+            Assert.Equal(src.Id, dest.Id);
+        }
+
+        [Fact]
+        public void DiscountPaymentDtoCanSetCreateDate()
+        {
+            var data = faker.Generate(2);
+            var src = data[0];
+            var dest = data[1];
+            dest.CreateDate = src.CreateDate;
+            Assert.Equal(src.CreateDate, dest.CreateDate);
+        }
+
+        [Theory]
+        [InlineData("Id")]
+        [InlineData("UserId")]
+        [InlineData("ExternalId")]
+        [InlineData("LevelName")]
+        [InlineData("PriceType")]
+        [InlineData("Price")]
+        [InlineData("CompletionDate")]
+        [InlineData("CreateDate")]
+        public void DiscountPaymentDtoHasExpectedFieldDefined(string name)
+        {
+            var sut = new DiscountPaymentDto();
+            var fields = sut.FieldList;
+            Assert.NotNull(fields);
+            Assert.NotEmpty(fields);
+            Assert.Contains(name, fields);
+        }
+
+        [Theory]
+        [InlineData("Id")]
+        [InlineData("UserId")]
+        [InlineData("ExternalId")]
+        [InlineData("LevelName")]
+        [InlineData("PriceType")]
+        [InlineData("Price")]
+        [InlineData("CompletionDate")]
+        [InlineData("CreateDate")]
+        public void DiscountPaymentDtoCanReadWriteByIndex(string fieldName)
+        {
+            var demo = faker.Generate();
+            var sut = new DiscountPaymentDto();
+            var flds = sut.FieldList;
+            var position = flds.IndexOf(fieldName);
+            sut[position] = demo[position];
+            var actual = sut[position];
+            Assert.Equal(demo[position], actual);
+        }
+    }
+}
