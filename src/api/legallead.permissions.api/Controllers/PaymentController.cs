81bf6a502e4909c686384f47aacf1eee36705d8b
Frank Thompson <frank.thompson.jr@gmail.com>
2024-06-04 09:54:24 -0500

    api - allow user to fetch payment processing setting

--- src/api/legallead.permissions.api/Controllers/PaymentController.cs
+++ src/api/legallead.permissions.api/Controllers/PaymentController.cs
@@ -5,6 +5,10 @@ namespace legallead.permissions.api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+
+    [SuppressMessage("Sonar Qube",
+    "S6960:Controller has multiple responsibilities",
+    Justification = "Payment controller actions are all related to payment processing details")]
     public class PaymentController(
         PaymentStripeOption payment,
         ISearchInfrastructure infra,
@@ -13,20 +17,40 @@ namespace legallead.permissions.api.Controllers
         private readonly PaymentStripeOption stripeOptions = payment;
         private readonly ISearchInfrastructure infrastructure = infra;
         private readonly IStripeInfrastructure stripeService = stripe;
+        
+        [HttpGet("payment-process-type")]
+        [ProducesResponseType<PaymentModeResponse>(StatusCodes.Status200OK)]
+        [ProducesResponseType<PaymentModeResponse>(StatusCodes.Status202Accepted)]
+        public IActionResult GetPaymentType()
+        {
+            const string live = "live";
+            var key = stripeOptions.Key;
+            var response = new PaymentModeResponse();
+            if (string.IsNullOrWhiteSpace(key)) { return Accepted(response); }
+            var isTest = !key.Contains(live, StringComparison.OrdinalIgnoreCase);
+            response.IsLive = !isTest;
+            response.Name = isTest ? "TEST" : "PROD";
+            return Ok(response);
+        }
 
         [HttpGet("product-codes")]
+        [ProducesResponseType<PaymentCode>(StatusCodes.Status200OK)]
         public IActionResult ProductCodes()
         {
             return Ok(stripeOptions.Codes);
         }
 
-        [HttpGet]
+        [HttpGet("session-status")]
+        [ProducesResponseType<object>(StatusCodes.Status200OK)]
         public IActionResult SessionStatus([FromQuery] string session_id)
         {
             return Ok(stripeService.SessionStatus(session_id));
         }
 
         [HttpPost("create-checkout-session")]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType<string>(StatusCodes.Status422UnprocessableEntity)]
+        [ProducesResponseType<object>(StatusCodes.Status200OK)]
         public async Task<IActionResult> Create(PaymentCreateRequest request)
         {
             var user = await infrastructure.GetUser(Request);
---
+++ src/api/legallead.permissions.api/Models/PaymentModeResponse.cs
@@ -0,0 +1,8 @@
+namespace legallead.permissions.api.Model
+{
+    public class PaymentModeResponse
+    {
+        public bool IsLive { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
