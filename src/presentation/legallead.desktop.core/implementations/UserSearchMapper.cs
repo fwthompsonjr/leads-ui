b0b4c684e0e6ee5d67931c4b821b57e5552e4029
Frank Thompson <frank.thompson.jr@gmail.com>
2024-06-18 23:10:52 -0500

    history - allow user to purchase

--- src/presentation/legallead.desktop.core/implementations/UserSearchMapper.cs
+++ src/presentation/legallead.desktop.core/implementations/UserSearchMapper.cs
@@ -171,7 +171,7 @@ namespace legallead.desktop.implementations
             }
         }
 
-        private const int tableRowCount = 20;
+        private const int tableRowCount = 15;
 
 
         private readonly static string historyhtml = Properties.Resources.searchhistory_table_html;
