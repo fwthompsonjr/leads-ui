6f8a4a84991fc71cb680fb61b7d4aba0d666214f
Frank Thompson <frank.thompson.jr@gmail.com>
2024-06-19 14:41:27 -0500

    search history - allow purchase

--- src/presentation/legallead.desktop/js/ViewHistoryJsHandler.cs
+++ src/presentation/legallead.desktop/js/ViewHistoryJsHandler.cs
@@ -1,4 +1,9 @@
 ﻿using CefSharp.Wpf;
+using legallead.desktop.entities;
+using legallead.desktop.handlers;
+using legallead.desktop.interfaces;
+using legallead.desktop.utilities;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 
 namespace legallead.desktop.js
@@ -8,9 +13,15 @@ namespace legallead.desktop.js
         public ViewHistoryJsHandler(ChromiumWebBrowser? browser) : base(browser) { }
         public override void Fetch(string id)
         {
-            if (!HasBrowser || web == null || user == null || !user.IsAuthenicated) return;
+            var provider = AppBuilder.ServiceProvider;
+            var permissionApi = provider?.GetService<IPermissionApi>();
+            if (!HasBrowser || web == null || user == null || !user.IsAuthenicated || permissionApi == null) return;
             if (string.IsNullOrEmpty(id) || !Guid.TryParse(id, out var _)) return;
-            base.Fetch(id);
+            var model = new GenerateInvoiceModel { Id = id };
+            var response = permissionApi.Post("search-get-invoice", model, user).Result;
+            if (response == null || response.StatusCode != 200) return;
+            var clspreview = new JsPreviewCompleted(web, response.Message);
+            clspreview.Invoice();
         }
     }
 }
