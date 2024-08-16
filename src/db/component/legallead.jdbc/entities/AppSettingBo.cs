cd23bf98477bc293b986df5ccc1a4ac035ad14eb
Frank Thompson <frank.thompson.jr@gmail.com>
2024-08-16 10:59:38 -0500

    feat: add application settings repository

---
+++ src/db/component/legallead.jdbc/entities/AppSettingBo.cs
@@ -0,0 +1,11 @@
+﻿namespace legallead.jdbc.entities
+{
+    public class AppSettingBo
+    {
+        public string? Id { get; set; }
+        public string? KeyName { get; set; }
+        public string? KeyValue { get; set; }
+        public decimal? Version { get; set; }
+        public DateTime? CreateDate { get; set; }
+    }
+}
