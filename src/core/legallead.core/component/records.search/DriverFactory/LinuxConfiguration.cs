16d6f32813f3f90da05e8122092114b2c0e3b1ce
Frank Thompson <frank.thompson.jr@gmail.com>
2024-09-01 11:52:07 -0500

    fix: registering driver service for linux driver

---
+++ src/core/legallead.core/component/records.search/DriverFactory/LinuxConfiguration.cs
@@ -0,0 +1,36 @@
+﻿namespace legallead.records.search.DriverFactory
+{
+    internal static class LinuxConfiguration
+    {
+
+        public static void Copy(string sourceDirectory, string targetDirectory)
+        {
+            if (!Directory.Exists(sourceDirectory)) return;
+            if (Directory.Exists(targetDirectory)) return;
+            var diSource = new DirectoryInfo(sourceDirectory);
+            var diTarget = new DirectoryInfo(targetDirectory);
+
+            CopyAll(diSource, diTarget);
+        }
+
+        public static void CopyAll(DirectoryInfo source, DirectoryInfo target)
+        {
+            Directory.CreateDirectory(target.FullName);
+
+            // Copy each file into the new directory.
+            foreach (FileInfo fi in source.GetFiles())
+            {
+                Console.WriteLine(@"Copying {0}\{1}", target.FullName, fi.Name);
+                fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
+            }
+
+            // Copy each subdirectory using recursion.
+            foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
+            {
+                DirectoryInfo nextTargetSubDir =
+                    target.CreateSubdirectory(diSourceSubDir.Name);
+                CopyAll(diSourceSubDir, nextTargetSubDir);
+            }
+        }
+    }
+}
