54b52598824ee93e3ba11818f0156884e6fc3978
Frank Thompson <frank.thompson.jr@gmail.com>
2024-11-16 14:23:49 -0600

    test: adding integration test for lead repository

---
+++ src/db/db.integration.tests/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+// assert that provider can get instance of ILeadUserRepository
+
+
+using db.integration.tests;
+using legallead.jdbc.interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+var instance = ServiceSetup.AppServices.GetRequiredService<ILeadUserRepository>();
+Console.WriteLine("Hello, World! {0}", instance.GetType().FullName);
---
+++ src/db/db.integration.tests/ServiceSetup.cs
@@ -0,0 +1,41 @@
+﻿using legallead.jdbc.helpers;
+using legallead.jdbc.implementations;
+using legallead.jdbc.interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace db.integration.tests
+{
+    internal static class ServiceSetup
+    {
+        public static IServiceProvider AppServices
+        {
+            get
+            {
+                while(Provider == null) { Init(); }
+                return Provider;
+            }
+        }
+        private static void Init()
+        {
+            if (Provider != null) return;
+            const string environ = "Test";
+            lock (locker)
+            {
+                var services = new ServiceCollection();
+                services.AddScoped<IDapperCommand, DapperExecutor>();
+                services.AddSingleton<IDataInitializer, DataInitializer>();
+                services.AddScoped(d =>
+                {
+                    var command = d.GetRequiredService<IDapperCommand>();
+                    var dbint = d.GetRequiredService<IDataInitializer>();
+                    return new DataContext(command, dbint, environ);
+                });
+                services.AddScoped<ILeadUserRepository, LeadUserRepository>();
+                Provider = services.BuildServiceProvider(); 
+            }
+        }
+
+        private static IServiceProvider? Provider = null;
+        private static readonly object locker = new();
+    }
+}
