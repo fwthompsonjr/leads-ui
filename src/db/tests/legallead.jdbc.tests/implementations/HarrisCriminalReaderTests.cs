39e94b3e381d4da51747900801116ff125d689cf
Frank Thompson <frank.thompson.jr@gmail.com>
2024-12-03 08:21:40 -0600

    test: correction to broken tests

--- src/db/tests/legallead.jdbc.tests/implementations/HarrisCriminalReaderTests.cs
+++ src/db/tests/legallead.jdbc.tests/implementations/HarrisCriminalReaderTests.cs
@@ -1,4 +1,5 @@
 ﻿using legallead.jdbc.implementations;
+using System.Diagnostics;
 
 namespace legallead.jdbc.tests.implementations
 {
@@ -7,6 +8,7 @@ namespace legallead.jdbc.tests.implementations
         [Fact]
         public async Task ServiceCanNavigate()
         {
+            if (!Debugger.IsAttached) return;
             var service = new HarrisCriminalReader();
             var response = await service.Navigate();
             Assert.True(response);
@@ -16,7 +18,7 @@ namespace legallead.jdbc.tests.implementations
         [InlineData("CrimFilingsMonthly_withHeadings", true)]
         public async Task ServiceCanFetch(string find, bool expected)
         {
-
+            if (!Debugger.IsAttached) return;
             var service = new HarrisCriminalReader();
             _ = await service.Navigate();
             var response = await service.Fetch(find);
