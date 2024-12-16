f1ece5cb9994f48e9704aa56c83357941e024743
Frank Thompson <frank.thompson.jr@gmail.com>
2024-12-15 19:26:58 -0600

    fix: user usage serialization change

--- src/db/component/legallead.jdbc/models/UserUsageAppendRecordModel.cs
+++ src/db/component/legallead.jdbc/models/UserUsageAppendRecordModel.cs
@@ -7,11 +7,15 @@ namespace legallead.jdbc.models
         [JsonProperty("lidx")] public string LeadUserId { get; set; } = string.Empty;
         [JsonProperty("cid")] public int CountyId { get; set; }
         [JsonProperty("ctname")] public string CountyName { get; set; } = string.Empty;
-        [JsonProperty("sdte")] public DateTime StartDate { get; set; }
-        [JsonProperty("edte")] public DateTime EndDate { get; set; }
+        [JsonIgnore] public DateTime StartDate { get; set; }
+        [JsonIgnore] public DateTime EndDate { get; set; }
         [JsonProperty("rc")] public int RecordCount { get; set; }
 
         [JsonProperty("drange")]
         public string DateRange => $"{StartDate:M/d} to {EndDate:M/d}";
+        [JsonProperty("sdte")] public string StartDateNode => $"{StartDate:yyyy-MM-dd}";
+        [JsonProperty("edte")] public string EndDateNode => $"{EndDate:yyyy-MM-dd}";
+
+
     }
 }
