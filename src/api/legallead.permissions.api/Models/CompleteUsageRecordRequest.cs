a6ed35749895a4849907c2cd51aca76ff5da0e32
Frank Thompson <frank.thompson.jr@gmail.com>
2024-12-26 23:21:49 -0600

    feat: allow user to save excel file name

--- src/api/legallead.permissions.api/Models/CompleteUsageRecordRequest.cs
+++ src/api/legallead.permissions.api/Models/CompleteUsageRecordRequest.cs
@@ -4,6 +4,6 @@ namespace legallead.permissions.api.Models
     {
         public string UsageRecordId { get; set; } = string.Empty;
         public int RecordCount { get; set; }
-        public string ExcelFileName { get; set; } = string.Empty;
+        public string ExcelName { get; set; } = string.Empty;
     }
 }
