fd5a97ea12911d93208260b5b029de1d456f8d10
Frank Thompson <Frank.Thompson@wolterskluwer.com>
2025-04-19 16:40:12 -0500

    feat: api expose methods to allow offline request management

---
+++ src/api/legallead.permissions.api/Models/UserOfflineStatusRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace legallead.permissions.api.Models
+{
+    public class UserOfflineStatusRequest
+    {
+        [Required]
+        [MinLength(32, ErrorMessage = "{0} must have a minimum length of {1} characters")]
+        [MaxLength(50, ErrorMessage = "{0} must have a maximum length of {1} characters")]
+        public string LeadId { get; set; } = string.Empty;
+    }
+}
